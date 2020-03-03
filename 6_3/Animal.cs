528e893064661fefd2583519174f488c20138f0e
seamo88 <bstockgame.88@gmail.com>
2020-03-03 12:26:56 -0600

    Add project files.

---
+++ 6_3/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6_3
+{
+    class Animal
+
+    {
+        private int legs { get; set; }
+        private int arms { get; set; }
+        private string type { get; set; }
+        private string name { get; set; }
+        private bool isDangerous { get; set; }
+
+
+
+        public Animal(int legs, int arms, string type, string name, bool isDangerous)
+        {
+            this.legs = legs;
+            this.arms = arms;
+            this.type = type;
+            this.name = name;
+            this.isDangerous = isDangerous;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Type of Animal: " + type);
+            Console.WriteLine("Number of Arms: " + arms);
+            Console.WriteLine("Number of Legs: " + legs);
+            Console.WriteLine("isDangerous?: " + isDangerous);
+
+        }
+
+        
+
+    }
+}
---
+++ 6_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6_3
+{
+    class Program
+
+    {
+        static void Main(String[]arg)
+        {
+            Animal animal1 = new Animal(4, 0, "Cat", "Snowball", true);
+            Animal animal2 = new Animal(8, 0, "Spider", "Friendly the Spider", false);
+            Animal animal3 = new Animal(2, 2, "Monkey", "Bobo", false);
+            animal1.ShowDetails();
+            animal2.ShowDetails();
+            animal3.ShowDetails();
+
+            CoffeeMugs mug1 = new CoffeeMugs("white", "black", "World's Best Daddy", 16);
+            CoffeeMugs mug2 = new CoffeeMugs("black", "gold", "Rise & Shine", 12);
+
+            mug1.Fill();
+            mug1.Drink();
+            mug1.Drink();
+            mug1.Fill();
+            mug1.Fill();
+            mug1.Drink();
+
+        }
+      
+
+       
+    }
+}
